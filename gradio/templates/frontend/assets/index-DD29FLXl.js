fcc934c8711d972d6064a9d8df95c102648aabcf
Ouassim Aouattah <arrobaouassim@gmail.com>
2025-01-21 13:42:25 +0100

    Add built frontend assets

---
+++ gradio/templates/frontend/assets/index-DD29FLXl.js
@@ -0,0 +1,2 @@
+import{E as o}from"./Embed-DlDdrAfU.js";import{b as p,c as r,l as m,m as n,p as t,a as _,s as g}from"./index-BXoNW3UD.js";import"./svelte/svelte.js";export{o as Embed,p as changeLocale,r as create_components,m as language_choices,n as mount_css,t as prefix_css,_ as process_langs,g as setupi18n};
+//# sourceMappingURL=index-DD29FLXl.js.map
---
+++ gradio/templates/frontend/gradio.js
@@ -0,0 +1,1 @@
+import("./assets/index-BXoNW3UD.js");
